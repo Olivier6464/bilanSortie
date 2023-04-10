3c551d5bf17818aa36a7d37a9b915ca3cb0f570a
olivier <olivier.ernst0411@orange.fr>
2023-04-10 22:15:58 +0200

    erreur typo index.html

--- js/main.js
+++ js/main.js
@@ -1,44 +1,43 @@
 
-  const makeReport = () => {
-    const besoin = document.querySelector("#besoin").value;
-    const output = document.querySelector("#output");
-    const civilite = document.querySelector("#genre").value;
-    const nom = document.querySelector("#nom").value;
-    const autonomie = document.querySelector("#autonomie").value;
-    const distance = document.querySelector("#distance").value;
+function makeReport(e) {
+    e.preventDefault();
+    let besoin = document.querySelector("#besoin").value;
+    let civilite = document.querySelector("#genre").value;
+    let nom = document.querySelector("#nom").value;
+    let autonomie = document.querySelector("#autonomie").value;
+    let distance = document.querySelector("#distance").value;
     let patient = "";
     let pronom = "";
     if (civilite == "Madame") {
-      patient = "la patiente";
-      pronom = "elle";
+        patient = "la patiente";
+        pronom = "elle";
     } else {
-      patient = "le patient";
-      pronom = "il"
+        patient = "le patient";
+        pronom = "il"
     }
     let transferts = getSelectionsListe("#transferts");
 
     let abesoin =
-      besoin == "est autonome"
-        ? "est autonome pour les transferts"
-        : `${besoin} pour les transferts de type ${transferts}`;
+        besoin == "est autonome"
+            ? "est autonome pour les transferts"
+            : `${besoin} pour les transferts de type ${transferts}`;
     let douleur = document.querySelector("#douleur").value;
     let location = document.querySelector("#location").value;
     if (douleur == "0/4") {
-      douleur = `${douleur} EVS`;
+        douleur = `${douleur} EVS`;
     } else {
-      douleur = `${douleur} EVS ${location}`;
+        douleur = `${douleur} EVS ${location}`;
     }
 
     // ====================== SORTIE DU TEXT =======================
 
-    output.innerHTML = `${civilite} ${nom} a bénéficié de séances de rééducation,
+    document.querySelector("#output").innerHTML = `${civilite} ${nom} a bénéficié de séances de rééducation,
 ${pronom} ${abesoin} 
 la douleur est estimée à ${douleur}.
 Au niveau de l'autonomie dans les déplacements ${patient} ${autonomie}, son périmètre de marche est d'environ ${distance}
-    `;
-    
-  };
-  document.querySelector("#genere").addEventListener("click", function auclick() {
-    makeReport();
-  });
+`;
+};
+
+document.querySelector("#genere").addEventListener("click", makeReport
+    ,false);
 
